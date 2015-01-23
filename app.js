528437bf071a1948c019dbf9d028974a4322d2f7
Chad Campbell <ccampb13@gmail.com>
2015-01-22 20:40:04 -0600

    removed unused dependencies, moved index to subfolder, removed unused font

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var debug = require('debug')('wedding');
+// var debug = require('debug')('wedding');
 
 var routes = require('./routes/index');
 
@@ -14,8 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -60,5 +57,5 @@ app.use(function(err, req, res, next) {
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + server.address().port);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Boyer-Campbell Wedding' });
+  res.render('pages/index', { title: 'Boyer-Campbell Wedding' });
 });
 
 module.exports = router;
