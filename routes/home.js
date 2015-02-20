1d40e0d0840a380a4f9d7cbfa6ae61bdc24bdeac
Chad Campbell <ccampb13@gmail.com>
2015-02-19 22:29:53 -0600

    EJCopyChanges

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ var home = express.Router();
 home.get('/', function(req, res) {
   res.render('home/index',
       {
-        title: 'EJ & Chad',
+        title: 'Chad & EJ',
         keywords: 'Wedding, Ceremony, Nashville, Houston Station, East End United Methodist Church, Summer Weddings',
         description: 'EJ Boyer & Chad Campbell are to be married August 8th in Nashville, TN. The ceremony will be held at East End United Methodist Church with the reception to immediately follow at Houston Station.',
         author: 'Chad Campbell, EJ Boyer, Peak Nine Designs.',
